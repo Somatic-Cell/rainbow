2811861bd38321a19ebc1fb0969324473e800eaf
soma-yokota <sy415@outlook.com>
2026-09-04 21:53:31 +0900

    Change directories

---
+++ shaders/entry/raindrop_trace.cu
@@ -0,0 +1,11 @@
+extern "C" __global__
+void __raygen__raindrop_trace();
+
+extern "C" __global__
+void __intersection__raindrop();
+
+extern "C" __global__
+void __closesthit__raindrop();
+
+extern "C" __global__
+void __miss__raindrop();
